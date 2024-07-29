f5db8f0c8593637ba4c2dc7dcff723462e7885ed
Pikens11 <gpique118@gmail.com>
2024-07-30 00:16:35 +0200

    Añadido componentes movie detail y movie list. Configurado las rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { MovieListComponent } from './components/movie-list/movie-list.component';
+import { MovieDetailComponent } from './components/movie-detail/movie-detail.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: MovieListComponent },
+  { path: 'item/:id', component: MovieDetailComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/services/omdb.service.ts
+++ src/app/services/omdb.service.ts
@@ -1,9 +1,34 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OmdbService {
 
-  constructor() { }
+  private apiUrl = 'http://www.omdbapi.com/';
+  private apiKey = '1c38e83b'; 
+
+  constructor(private http: HttpClient) { }
+
+  getMovies(search: string, page: number, filters: any): Observable<any> {
+    let params: any = {
+      s: search,
+      page: page.toString(),
+      apikey: this.apiKey,
+      type: filters.type || '',
+      y: filters.year || '',
+      genre: filters.genre || ''
+    };
+    console.log('Fetching movies with params:', params);
+    return this.http.get<any>(this.apiUrl, { params });
+  }
+
+  getMovieDetail(id: string): Observable<any> {
+    console.log('Fetching movie detail with ID:', id);
+    return this.http.get<any>(this.apiUrl, { params: { i: id, apikey: this.apiKey } });
+  }
 }
+
+
