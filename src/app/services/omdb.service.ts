ed8dbb3e1756d1de7a354e742cf5e9c6ab4dc59d
Pikens11 <gpique118@gmail.com>
2024-07-30 00:31:40 +0200

    implementado el servicio omdb api para su manejo

--- src/app/services/omdb.service.ts
+++ src/app/services/omdb.service.ts
@@ -1,34 +1,24 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OmdbService {
-
   private apiUrl = 'http://www.omdbapi.com/';
-  private apiKey = '1c38e83b'; 
+  private apiKey = '1c38e83b'
 
   constructor(private http: HttpClient) { }
 
-  getMovies(search: string, page: number, filters: any): Observable<any> {
-    let params: any = {
-      s: search,
-      page: page.toString(),
-      apikey: this.apiKey,
-      type: filters.type || '',
-      y: filters.year || '',
-      genre: filters.genre || ''
-    };
-    console.log('Fetching movies with params:', params);
-    return this.http.get<any>(this.apiUrl, { params });
+  searchMovies(title: string, type: string = '', year: string = '', page: number = 1): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}?apikey=${this.apiKey}&s=${title}&type=${type}&y=${year}&page=${page}`);
   }
 
-  getMovieDetail(id: string): Observable<any> {
-    console.log('Fetching movie detail with ID:', id);
-    return this.http.get<any>(this.apiUrl, { params: { i: id, apikey: this.apiKey } });
+  getMovieDetails(id: string): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}?apikey=${this.apiKey}&i=${id}&plot=full`);
   }
 }
+
 
 
